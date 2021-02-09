0459a226e62f09fc98522914f6401a39ea55e88c
Andrett Duvenhage <andrettiduv@gmail.com>
2021-02-09 15:46:50 -0800

    log auth url to spot

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Login.css';
+import { loginUrl } from "./spot";
 
 function Login() {
     return (
         <div className="login">
             <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-green-logo-8.png" alt="spot logo"/>
             
-            <a>LOGIN WITH SPOT</a>
+            <a href={loginUrl}>LOGIN WITH SPOT</a>
   
         </div>
     );
---
+++ src/spot.js
@@ -0,0 +1,20 @@
+  // doc to get up and running
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "d8d53565bd0342e88e5903766cd88ce1"
+
+
+  //permitions state
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+  ];
+
+  export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
