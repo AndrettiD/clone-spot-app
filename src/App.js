3314153d1b71335facfeb400f1720eee47ecb5a7
Andrett Duvenhage <andrettiduv@gmail.com>
2021-02-09 16:36:49 -0800

    acces token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from "./spot"
 
 function App() {
+  
+  //run code on a given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+  }, []);
+
   return (
      //BEM
-     <div className="app">      
-      
+     <div className="app">
      <Login />
    </div>
  );
